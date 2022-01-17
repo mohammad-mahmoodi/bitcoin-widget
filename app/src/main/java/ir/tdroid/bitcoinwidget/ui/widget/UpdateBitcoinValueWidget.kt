f1606db24a09aa2e64467557e5840373acf34a14
mohammad <sm.mahmoodi@gmail.com>
2022-01-18 00:00:12 +0330

    improve widget view

--- app/src/main/java/ir/tdroid/bitcoinwidget/ui/widget/UpdateBitcoinValueWidget.kt
+++ app/src/main/java/ir/tdroid/bitcoinwidget/ui/widget/UpdateBitcoinValueWidget.kt
@@ -3,6 +3,7 @@ package ir.tdroid.bitcoinwidget.ui.widget
 import android.appwidget.AppWidgetManager
 import android.content.ComponentName
 import android.content.Context
+import android.view.View
 import android.widget.RemoteViews
 import dagger.hilt.android.qualifiers.ApplicationContext
 import ir.tdroid.bitcoinwidget.R
@@ -13,10 +14,10 @@ class UpdateBitcoinValueWidget @Inject constructor(
     ) {
     fun updateBitcoinPrice(bitcoinPrice : Double) {
         val appWidgetManager = AppWidgetManager.getInstance(context)
-        val widgetText = bitcoinPrice.toString()
+        val price = String.format("%.2f", bitcoinPrice ) + " $"
         val views = RemoteViews(context.packageName, R.layout.bitcoin_value_widget)
-        views.setTextViewText(R.id.appwidget_text, widgetText)
-
+        views.setTextViewText(R.id.bitcoinPrice, price)
+        views.setViewVisibility(R.id.progressBar , View.GONE)
         appWidgetManager.updateAppWidget(
             ComponentName(
                 context.packageName,
